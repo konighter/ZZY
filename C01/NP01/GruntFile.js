60d812696ff7f8dc7d3559864a93127c6e30c94f
bin <konighter@126.com>
2014-09-24 16:51:56 +0800

    mod grunt and package.json

--- C01/NP01/GruntFile.js
+++ C01/NP01/GruntFile.js
@@ -2,15 +2,16 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['<%= pkg.jspath %>/*.js' , '<%= pkg.jslib %>/**/*.js'],
-                dest: '<%= pkg.jspath %>/<%= pkg.name %>.js'
-            }
-        },
+        vendor : grunt.file.readJSON('public/bower.json'),
+//        concat: {
+//            options: {
+//                separator: ';'
+//            },
+//            dist: {
+//                src: ['<%= pkg.jspath %>/*.js' , '<%= pkg.jslib %>/**/*.js' ,'!<%= pkg.jspath %>/*.min.js'],
+//                dest: '<%= pkg.jspath %>/<%= pkg.name %>.js'
+//            }
+//        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
@@ -21,11 +22,27 @@ module.exports = function(grunt) {
                 }
             }
         },
-      
-        watch: {
-            files: <%= concat.dist.src %>,
-            tasks: ['default']
+        clean: ["<%= pkg.jspath %>/<%= pkg.name %>.js","<%= pkg.jspath %>/<%= pkg.name %>.min.js"],
+        copy : {
+            bootstrap:{
+                expand: true,
+                cwd: '<%= pkg.jslib %>/bootstrap/dist/',
+                src: ['css/*.min.css','fonts/*','js/*.min.js'] ,
+                dest: '<%= pkg.jsvendor %>/bootstrap/',
+                filter: 'isFile'
+            },
+            jquery:{
+                expand: true,
+                cwd: '<%= pkg.jslib %>/jquery/dist/',
+                src: ['css/*.min.css','fonts/*','js/*.min.js,','*.min.js'] ,
+                dest: '<%= pkg.jsvendor %>/jquery/',
+                filter: 'isFile'
+            }
         },
+//        watch: {
+//            files: ['<%= concat.dist.src %>'],
+//            tasks: ['default']
+//        },
         compass: {
             production: {
                 options: {
@@ -47,17 +64,15 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+//    grunt.loadNpmTasks('grunt-contrib-jshint');
+//    grunt.loadNpmTasks('grunt-contrib-qunit');
+//    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.registerTask('test', [ 'qunit']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', [ 'concat', 'uglify']);
+    grunt.registerTask('default', [ 'clean', 'uglify']);
 
-    grunt.event.on('watch', function(action, filepath, target) {
-        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
-    });
 
 };
